300b6b069581466bf1d23e70d2137661bc9367e2
pesekon2 <pesej.ondrek@gmail.com>
2024-01-31 09:57:06 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # GrassPT
 GPT helper for GRASS GIS
+
+## Usage
+
+Build the docker file (it is recommended to run it with nohup as the training takes some time):
+
+```
+nohup docker build -t grasspt:1.0 -f docker/Dockerfile . &
+```
+
+Ask a query:
+
+```
+docker run --rm --gpus all grasspt:1.0 python3 /src/query.py " How to get metadata of a raster map in GRASS GIS?"
+```
---
+++ src/query.py
@@ -0,0 +1,45 @@
+import argparse
+from transformers import PreTrainedTokenizerFast, GPT2LMHeadModel, GPT2TokenizerFast, GPT2Tokenizer
+
+def load_model(model_path):
+    model = GPT2LMHeadModel.from_pretrained(model_path)
+    return model
+
+
+def load_tokenizer(tokenizer_path):
+    tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
+    return tokenizer
+
+def generate_text(model_path, sequence, max_length):
+
+    model = load_model(model_path)
+    tokenizer = load_tokenizer(model_path)
+    ids = tokenizer.encode(f'{sequence}', return_tensors='pt')
+    final_outputs = model.generate(
+        ids,
+        do_sample=True,
+        max_length=max_length,
+        pad_token_id=model.config.eos_token_id,
+        top_k=50,
+        top_p=0.95,
+        # num_return_sequences=1
+        # pad_token_id=tokenizer.eos_token_id
+    )
+    print(tokenizer.decode(final_outputs[0], skip_special_tokens=True))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Ask query')
+
+    parser.add_argument(
+        '--query',
+        type=str,
+        default='How to get the manual for a certain module in GRASS GIS?',
+        help='The query you would like to ask about GRASS GIS'
+    )
+
+    args = parser.parse_args()
+
+    model2_path = "/tmp/m"
+    max_len = 800
+    generate_text(model2_path, args.query, max_len)
